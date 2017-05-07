f967558f52822245dbacdafdba65a3646189e233
Adrian Abela <aadrianabela@gmail.com>
2017-05-07 11:11:43 +0200

    feat: tied up article.service to proper endpoint

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -1,71 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Article } from './article';
 import { Observable } from "rxjs/Observable";
+import { Settings } from '../settings';
+import { Http } from '@angular/http';
+
 import 'rxjs/add/observable/fromPromise';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class ArticleService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
-  private dummyArticle = {
-    "title": "Crisis in Llamatown",
-    "tease": "A crisis has occured in Llamatown when el presidente shot a llama for no reason",
-    "sections": [
-      {
-        "title": "Background",
-        "content": "Llamas are awesome creatures",
-        "references": [
-          {
-            "name": "Llamas are awesome",
-            "source": "Llama News Network",
-            "url": "http://www.example.com"
-          }
-        ]
-      },
-      {
-        "title": "Conclusion",
-        "content": "Llamas are still awesome creatures",
-        "references": [
-          {
-            "name": "Llamas are still awesome",
-            "source": "Llama News Network",
-            "url": "http://www.example.com/awesome"
-          }
-        ]
-      }
-    ],
-    "tags": [
-      "llama",
-      "pj",
-      "crisis",
-      "llamatown"
-    ],
-    "links": {
-      "people": [
-        {
-          "name": "Juliet Borg",
-          "category": "accused"
-        },
-        {
-          "name": "Marphy",
-          "category": "accuser"
-        }
-      ],
-      "articles": [
-        {
-          "name": "Something that once happened",
-          "category": "further reading"
-        }
-      ]
-    },
-    "date": new Date("2017-01-01T00:00:00.000+0000"),
-    "dateCreated": new Date("2017-02-01T00:00:00.000+0000"),
-    "lastModified": new Date("2017-03-01T00:00:00.000+0000")
-  }
+  private url = Settings.host + "/articles";
 
   getArticle(name: string): Observable<Article> {
-    return Observable.fromPromise
-      (Promise.resolve(new Article(this.dummyArticle)));
+    if (!name) {
+      return null;
+    }
+
+    return Observable.fromPromise(this.http.get(this.url + "/" + name).toPromise().then((res) => {
+        return Promise.resolve(new Article(res.json()));
+    }));
   }
 }
